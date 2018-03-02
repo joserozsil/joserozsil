0380bf2f12f659bd888e56a10c5bab998e9beb70
Jose Rodriguez <jseanvzla@gmail.com>
2018-03-02 01:04:58 -0400

    [Test] Test #1

--- js/vue.js
+++ js/vue.js
@@ -1,4 +1,8 @@
-axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, Content-Type, X-Auth-Token';
+
 
 var app = new Vue({
     el: '#index',
@@ -7,8 +11,9 @@ var app = new Vue({
         emulateHTTP: true
     },
     mounted() {
+
+
         this.saveView()
-        console.log(new Date().getTime())
     },
     data: {
         database: firebase.database()
@@ -24,6 +29,27 @@ var app = new Vue({
                         views: value + 1
                     })
                 })
+            firebase.database().ref('/statistics/').once('value')
+                .then((snapshot) => {
+                    let data = []
+                    axios.get('http://api.wipmania.com/json')
+                        .then((response) => {
+                            console.log(response.data);
+                            data = response.data;
+
+                        })
+                        .catch(function(error) {
+                            console.log(error);
+                        });
+
+                    let object = {
+                        "country": data.address.country,
+                        "lat": data.latitude,
+                        "long": data.longitude
+                    };
+                    firebase.database().ref('statistics/country').push(object)
+                })
+
         },
         downloadCv() {
             firebase.database().ref('/statistics/').once('value')
